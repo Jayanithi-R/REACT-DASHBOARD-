5d277934fb2b15f8e66fa7e5472427a0fcbf8547
JAYANITHI R MTECH CSE <jayanithi.2253023@srec.ac.in>
2025-09-30 10:35:06 +0000

    screenshot la left to right la epidi iruko athe mathiri venum  intern sh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,20 +30,16 @@ export default function Home() {
             <DashboardHeader />
           </header>
           <main className="flex-1 p-4 sm:p-6 lg:p-8">
-            <div className="flex flex-col gap-6">
-              <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
-                <div className="xl:col-span-2 flex flex-col gap-6">
-                  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <AttendanceReport attendance={attendance} />
-                    <Tasks tasks={tasks} />
-                  </div>
-                  <LeaveRequests leaveRequests={leaveRequests} />
+            <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+              <div className="xl:col-span-2 flex flex-col gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <AttendanceReport attendance={attendance} />
+                  <Tasks tasks={tasks} />
                 </div>
-                <div className="xl:col-span-1 flex flex-col gap-6">
-                  <SchedulePanel schedule={schedule} />
-                </div>
+                <LeaveRequests leaveRequests={leaveRequests} />
               </div>
-              <div className="grid grid-cols-1 gap-6">
+              <div className="xl:col-span-1 flex flex-col gap-6">
+                <SchedulePanel schedule={schedule} />
                 <InternshipCard />
               </div>
             </div>
