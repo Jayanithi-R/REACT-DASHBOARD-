b2791887f478a8412b080a7e2fd50f23e699a685
JAYANITHI R MTECH CSE <jayanithi.2253023@srec.ac.in>
2025-09-30 10:25:48 +0000

    schedule conntainer box nala keela varanum screenshot la mention panna m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             <DashboardHeader />
           </header>
           <main className="flex-1 p-4 sm:p-6 lg:p-8">
-            <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 xl:grid-cols-3 gap-6 items-start">
               <div className="xl:col-span-2 flex flex-col gap-6">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                   <AttendanceReport attendance={attendance} />
