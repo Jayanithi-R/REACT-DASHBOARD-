e2c5eb4433b6003f5471d6ee03c46260c7519a82
JAYANITHI R MTECH CSE <jayanithi.2253023@srec.ac.in>
2025-10-03 10:08:23 +0000

    schedule , internship containerum one by one irukanum side la irukanum s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,19 @@ export default function Home() {
             <DashboardHeader />
           </header>
           <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
-            <div className="space-y-6">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <AttendanceReport attendance={attendance} />
-                <Tasks tasks={tasks} />
-              </div>
-              <div>
-                <LeaveRequests leaveRequests={leaveRequests} />
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
-                <div className="lg:col-span-3">
-                  <SchedulePanel schedule={schedule} />
+            <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+              <div className="lg:col-span-3 space-y-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <AttendanceReport attendance={attendance} />
+                  <Tasks tasks={tasks} />
                 </div>
-                <div className="lg:col-span-2">
-                  <InternshipCard />
+                <div>
+                  <LeaveRequests leaveRequests={leaveRequests} />
                 </div>
+              </div>
+              <div className="lg:col-span-2 space-y-6">
+                <SchedulePanel schedule={schedule} />
+                <InternshipCard />
               </div>
             </div>
           </main>
